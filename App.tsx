5cacaf345d10e7fda7c34515affcffde1c0755fa
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-20 20:43:28 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native"; // esse elemento é nativo do react-native que é específico para contexto mobile - View é como se fosse uma div
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  // App é a função padrão que vai ser iniciada no nosso app
+  // no JSX um componente é uma interface e também é uma função que tem um retorno onde fica os elementos que serão exibidos em tela
+  return ( // um componente só pode retornar um elemento -> todos os elementos tem que ficar cercados por alguma tag
+    <View>
+      <Text>Cuidado para não ser corno!! :)</Text>
+      <Text>Você pode achar que não é, mas quem sabe ;)</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
