894a34f0145b06761bcc9ddfa866d5bd5b4d6944
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-21 11:53:24 -0300

    add button in page Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,30 @@
-import { Text, View, TextInput } from "react-native"; // esse elemento é nativo do react-native que é específico para contexto mobile - View é como se fosse uma div
+import { Text, View, TextInput, TouchableOpacity } from "react-native"; // esse elemento é nativo do react-native que é específico para contexto mobile - View é como se fosse uma div
 import { styles } from "./styles";
 
 export function Home() {
   // App é a função padrão que vai ser iniciada no nosso app
   // no JSX um componente é uma interface e também é uma função que tem um retorno onde fica os elementos que serão exibidos em tela
+  function handleParticipantAdd() {
+    console.log("vá se lascar!")
+  }
+  
   return (
     // um componente só pode retornar um elemento -> todos os elementos tem que ficar cercados por alguma tag
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
 
-      <TextInput 
-        style={styles.input} 
+      <TextInput
+        style={styles.input}
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        {/*esse elemento é para indicar que aqui é uma região clicável da aplicação*/}
+        <Text style={styles.buttonText}>+</Text>
+        {/*temos que colocar o elemento Text para não dar erro*/}
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -17,7 +17,6 @@ export const styles = StyleSheet.create({
     color: "#6B6B6B",
     fontSize: 16,
   },
-
   input: {
     height: 56,
     backgroundColor: '#1F1E25',
@@ -25,5 +24,17 @@ export const styles = StyleSheet.create({
     color: '#FFF',
     padding: 16,
     fontSize: 16,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center', // o flexbox já está ativado
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24
   },
 });
