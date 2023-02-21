93185edc1dc97e0b4e4bfa71ee5083595eb61fe0
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-21 09:41:43 -0300

    add input in page Home

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import Home from "./src/screens/Home";
+import { Home } from "./src/screens/Home";
 
-export default function App() {
+export default function App() { // aqui é obrigatório ter o default porque é o arquivo App (primeiro a ser procurado pra ser renderizado)
   return (
     <Home />
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
-import { Text, View } from "react-native"; // esse elemento é nativo do react-native que é específico para contexto mobile - View é como se fosse uma div
+import { Text, View, TextInput } from "react-native"; // esse elemento é nativo do react-native que é específico para contexto mobile - View é como se fosse uma div
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
   // App é a função padrão que vai ser iniciada no nosso app
   // no JSX um componente é uma interface e também é uma função que tem um retorno onde fica os elementos que serão exibidos em tela
   return (
@@ -9,6 +9,12 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+      <TextInput 
+        style={styles.input} 
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
     </View>
   );
 }
