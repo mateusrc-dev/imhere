32a8316a8128e1d00cf4c03f1f4b85e2ddf7241d
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-21 12:16:05 -0300

    update page Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,26 +5,28 @@ export function Home() {
   // App é a função padrão que vai ser iniciada no nosso app
   // no JSX um componente é uma interface e também é uma função que tem um retorno onde fica os elementos que serão exibidos em tela
   function handleParticipantAdd() {
-    console.log("vá se lascar!")
+    console.log("vá se lascar!");
   }
-  
+
   return (
     // um componente só pode retornar um elemento -> todos os elementos tem que ficar cercados por alguma tag
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        {/*esse elemento é para indicar que aqui é uma região clicável da aplicação*/}
-        <Text style={styles.buttonText}>+</Text>
-        {/*temos que colocar o elemento Text para não dar erro*/}
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          {/*esse elemento é para indicar que aqui é uma região clicável da aplicação*/}
+          <Text style={styles.buttonText}>+</Text>
+          {/*temos que colocar o elemento Text para não dar erro*/}
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
